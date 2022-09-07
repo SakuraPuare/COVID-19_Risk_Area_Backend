c1c52e305797e537cbf8f7cd48ad244714a64488
Moder Steven <java20131114@gmail.com>
2022-09-07 21:36:58 +0800

    ✨ feat: 加载最后数据

--- version.py
+++ version.py
@@ -1,11 +1,21 @@
 import json
 import pathlib
-from typing import Dict, List
+from typing import Dict, List, Union
+
+import query
 
 path = pathlib.Path('version')
 
 if not path.exists() and not path.is_dir():
 	path.mkdir()
+
+
+def list_version() -> Union[List, None]:
+	file_list = list(path.iterdir())
+	if len(file_list) == 0:
+		return None
+	else:
+		return sorted([str(i).split('.')[0] for i in file_list if i.is_file()])
 
 
 def save_version(version: str, area: Dict[str, Dict]) -> None:
@@ -14,15 +24,30 @@ def save_version(version: str, area: Dict[str, Dict]) -> None:
 
 
 def load_version(version) -> Dict[str, List]:
-	with open(pathlib.Path(f"{version}.json"), 'r') as f:
+	with open(path / pathlib.Path(f"{version}.json"), 'r',encoding='utf-8') as f:
 		return json.loads(f.read())
 
 
 def has_version(version: str) -> bool:
 	file_list = path.iterdir()
-	file = pathlib.Path(version + '.json')
-	if file in file_list:
+	file = path / pathlib.Path(version + '.json')
+	if len(list(file_list)) == 0:
+		return False
+	if file in list(path.iterdir()):
 		if file.exists() and file.is_file():
 			return True
 		else:
 			return False
+
+
+def load_last_version() -> Dict[str, list]:
+	version_list = list_version()
+	if version_list is None:
+		return query.main()
+	else:
+		return version_list[-1]
+
+
+if __name__ == '__main__':
+	v = load_last_version()
+	pass
