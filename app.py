4b26b57026017b583057513885984bf472873d92
Moder Steven <java20131114@gmail.com>
2022-09-08 23:41:44 +0800

    🦄 refactor: API修改

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ risk_data = version.load_last_version()
 app = Flask(__name__)
 
 
-@app.route('/admin/refresh')
+@app.route('/admin/refresh/')
 def refresh_admin():
 	global risk_data
 	risk_data = query.main()
@@ -27,7 +27,7 @@ def forbidden():
 	return "Forbidden", 403
 
 
-@app.route('/api/search/<string:area>')
+@app.route('/time')
 def api_time():
 	v = [i.split('/')[-1] for i in version.list_version()]
 	return json.dumps(v)
@@ -47,7 +47,7 @@ def api_search(area):
 
 @app.route("/")
 def hello_world():
-	return {'msg': 'Hey! This is just an api', 'status': 200}
+	return 'Hey! This is just an api'
 
 
 if __name__ == "__main__":
